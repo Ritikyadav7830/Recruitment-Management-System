4a147d4485b524c7550bcf8ecc34e76535052109
Ritik Yadav <ritik783026yadav@gmail.com>
2026-06-21 22:33:06 +0530

    update frontend CaseSensitive name

--- Frontend/src/pages/AdminLogin.jsx
+++ Frontend/src/pages/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 import { useDispatch } from "react-redux";
-import { login } from "../redux/authSlice";
+import { login } from "../redux/AuthSlice";
 import { API_URL } from "../config";
 import axios from "axios"
 
